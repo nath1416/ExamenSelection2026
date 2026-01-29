9b651df67b9c90a2b494ebd65d78af43d504a4e1
ptrstr <ptrstr@protonmail.com>
2026-01-29 15:29:55 -0500

    add dumb shaders

--- Shaders/shaders/wavePlane/wavePlane.fs.glsl
+++ Shaders/shaders/wavePlane/wavePlane.fs.glsl
@@ -10,19 +10,14 @@ out vec4 FragColor;
 uniform vec3 viewPos;
 uniform float time;
 
-uniform vec3 waveColor1;  
-uniform vec3 waveColor2;  
+uniform vec3 waveColor1;
+uniform vec3 waveColor2;
 uniform float waveAmplitude;
 
 void main() {
-    // TODO: Finish the fragment shader implementation
+    float factor = clamp((-waveAmplitude / (WaveHeight - 2 * waveAmplitude)) - 0.5, 0.0, 1.0);
 
-    vec3 ambient = vec3(0.1, 0.1, 0.1);
-    vec3 diffuse = vec3(0.4, 0.4, 0.4);
-    vec3 specular = vec3(0.5, 0.5, 0.5);
-    vec3 fresnelColor = vec3(0.2, 0.5, 0.8);
-    
-    vec3 result = ambient + diffuse + specular + fresnelColor * 0.3;
-    
+    vec3 result = mix(waveColor1, waveColor2, factor);
+
     FragColor = vec4(result, 0.9);
 }
--- Shaders/shaders/wavePlane/wavePlane.vs.glsl
+++ Shaders/shaders/wavePlane/wavePlane.vs.glsl
@@ -18,29 +18,38 @@ uniform float waveAmplitude;
 uniform float waveFrequency;
 uniform float waveSpeed;
 
+const float PI = 3.14159265358979323826264;
+const float G = 9.81;
+
+float wave(vec2 direction) {
+    float k = 2.0 * PI * waveFrequency;
+    float angularFreq = sqrt(G * k);
+
+    float phase = k * (dot(aPos.xz, direction)) - (angularFreq * waveSpeed * time);
+
+    return waveAmplitude * sin(phase);
+}
+
 void main() {
-    // TODO: Finish the vertex shader implementation
     // You need to create a realistic wave effect on the plane
 
-    float wave1 = 0.3;
-    float wave2 = 0.3;
-    float wave3 = 0.3;
-    
-    float totalWave = wave1 + wave2 + wave3;
-    
+    float totalWave = (wave(vec2(0, cos(time))) +
+        wave(vec2(cos(time), sin(time))) +
+        wave(vec2(0.78, 0.2 * cos(time))));
+
     vec3 displacedPos = aPos;
-    displacedPos.y += totalWave;
-    
+    displacedPos.y += totalWave * 0.05;
+
     float dx = 1;
 
     float dz = 1;
-    
+
     vec3 calculatedNormal = normalize(vec3(-dx, 1.0, -dz));
-    
+
     FragPos = vec3(model * vec4(displacedPos, 1.0));
     Normal = mat3(transpose(inverse(model))) * calculatedNormal;
     TexCoord = aTexCoord;
     WaveHeight = totalWave;
-    
+
     gl_Position = projection * view * model * vec4(displacedPos, 1.0);
 }
